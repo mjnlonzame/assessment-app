537b0fbc445f3845e1a12dfd49a4750b87b3d752
MJ <mjnlonzame@gmail.com>
2021-11-20 23:40:09 +0800

    test prod

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
+const cors = require('cors');
 
 app = express();
+app.use(cors());
 app.use(serveStatic(`${__dirname}/dist`));
 const port = process.env.PORT || 3000;
 app.listen(port);
